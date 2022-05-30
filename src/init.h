3b1014832f4ae0dd9219c8eabb1cc9e58224dc48
ProgKea <hoschy01@googlemail.com>
2022-05-30 15:00:22 +0200

    Initial Commit

---
+++ src/init.h
@@ -0,0 +1,17 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
+#define HEIGHT 900
+#define RESOLUTION 16 / 9
+#define WIDTH 900 * RESOLUTION
+
+extern SDL_Window *win;
+extern SDL_Renderer *renderer;
+
+void init_sdl2();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "header.h"
+
+int main(void) {
+  init_sdl2();
+
+  while (1) {
+    SDL_Event e;
+    if (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT)
+        break;
+      if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
+        break;
+    }
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 28, 28, 28, 255);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyWindow(win);
+  SDL_DestroyRenderer(renderer);
+
+  return 0;
+}
