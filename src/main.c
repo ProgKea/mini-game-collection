f3a1e4dfb8e3b56e80fda414fadd18b077781a20
ProgKea <hoschy01@googlemail.com>
2022-07-17 10:35:36 +0200

    changed indentation

--- src/main.c
+++ src/main.c
@@ -3,51 +3,52 @@
 game_state active_game_state = menu_state;
 bool quit = false;
 
-int main(void) {
-  init_sdl2();
+int main(void)
+{
+    init_sdl2();
 
-  MenuItems menu = create_menu(renderer, WIDTH);
+    MenuItems menu = create_menu(renderer, WIDTH);
 
-  FlappyBird flappybird = create_bird(renderer, WIDTH, HEIGHT);
+    FlappyBird flappybird = create_bird(renderer, WIDTH, HEIGHT);
 
-  Breakout breakout = create_breakout(renderer, WIDTH, HEIGHT);
+    Breakout breakout = create_breakout(renderer, WIDTH, HEIGHT);
 
-  while (!quit) {
-    SDL_Event e;
-    if (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT)
-        break;
-      switch (active_game_state) {
-      case menu_state:
-        menu_events(e, renderer, &menu, WIDTH);
-        break;
-      case flappybird_state:
-        events_flappybird(e, renderer, WIDTH, HEIGHT, &flappybird);
-        break;
-      case breakout_state:
-        events_breakout(e, renderer, WIDTH, HEIGHT, &breakout);
-        break;
-      }
+    while (!quit) {
+        SDL_Event e;
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+            break;
+            switch (active_game_state) {
+                case menu_state:
+                menu_events(e, renderer, &menu, WIDTH);
+                break;
+                case flappybird_state:
+                events_flappybird(e, renderer, WIDTH, HEIGHT, &flappybird);
+                break;
+                case breakout_state:
+                events_breakout(e, renderer, WIDTH, HEIGHT, &breakout);
+                break;
+            }
+        }
+
+        switch (active_game_state) {
+            case menu_state:
+            render_menu(renderer, &menu);
+            break;
+            case flappybird_state:
+            update_flappybird(WIDTH, HEIGHT, &flappybird);
+            render_flappybird(renderer, flappybird);
+            break;
+            case breakout_state:
+            update_breakout(WIDTH, HEIGHT, &breakout);
+            render_breakout(renderer, breakout);
+            break;
+        }
     }
 
-    switch (active_game_state) {
-    case menu_state:
-      render_menu(renderer, &menu);
-      break;
-    case flappybird_state:
-      update_flappybird(WIDTH, HEIGHT, &flappybird);
-      render_flappybird(renderer, flappybird);
-      break;
-    case breakout_state:
-      update_breakout(WIDTH, HEIGHT, &breakout);
-      render_breakout(renderer, breakout);
-      break;
-    }
-  }
-
-  SDL_DestroyWindow(win);
-  SDL_DestroyRenderer(renderer);
-  destroy_menu(menu);
-  destroy_flappybird(flappybird);
-  return 0;
+    SDL_DestroyWindow(win);
+    SDL_DestroyRenderer(renderer);
+    destroy_menu(menu);
+    destroy_flappybird(flappybird);
+    return 0;
 }
